cf8223e3089ebc1b38cce82b0d1180362e93b678
Lieven Dekeyser <lievendekeyser@gmail.com>
2021-06-14 13:32:28 +0200

    Filter the images in the grid view

--- PhotographicHistory/PhotoGridViewController.swift
+++ PhotographicHistory/PhotoGridViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 import Photos
 import Vision
 
+enum HistoricPhotoMatch {
+	enum NoMatchReasons: String {
+		case noLocation
+		case containsPeople
+		case notOutdoors
+		case isDocument
+		case containsLicensePlate
+	}
+	case matches
+	case noMatch([NoMatchReasons])
+}
+
 
 class PhotoCell: UICollectionViewCell {
 	static let reuseIdentifier = "PhotoCell"
@@ -36,8 +48,16 @@ class PhotoCell: UICollectionViewCell {
 }
 
 
-class ViewController: UICollectionViewController {
-	let fetchResult: PHFetchResult<PHAsset>
+class PhotoGridViewController: UICollectionViewController {
+	let allAssets: [PHAsset]
+	var remainingAssets: [PHAsset] = []
+	
+	var filteredAssets: [PHAsset] {
+		didSet {
+			collectionView.reloadData()
+		}
+	}
+	
 	let imageManager = PHCachingImageManager()
 	var availableWidth: CGFloat = 0.0
 	
@@ -45,31 +65,71 @@ class ViewController: UICollectionViewController {
 	
 	let layout: UICollectionViewFlowLayout = {
 		let layout = UICollectionViewFlowLayout()
-		layout.itemSize = CGSize(width: ViewController.minThumbnailSize, height: ViewController.minThumbnailSize)
+		layout.itemSize = CGSize(width: PhotoGridViewController.minThumbnailSize, height: PhotoGridViewController.minThumbnailSize)
 		layout.minimumLineSpacing = 8.0
 		layout.minimumInteritemSpacing = 8.0
 		layout.sectionInset = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: 8.0)
 		return layout
 	}()
 	
-	init(fetchResult: PHFetchResult<PHAsset>) {
-		self.fetchResult = fetchResult
-		
+	convenience init(fetchResult: PHFetchResult<PHAsset>) {
+		var assets = [PHAsset]()
+		fetchResult.enumerateObjects { (asset, _, _) in
+			assets.append(asset)
+		}
+		self.init(assets: assets)
+	}
+	
+	init(assets: [PHAsset]) {
+		self.allAssets = assets
+		self.filteredAssets = assets
 		super.init(collectionViewLayout: layout)
 	}
 	
 	convenience init() {
 		let options = PHFetchOptions()
-		options.fetchLimit = 1000
+		options.fetchLimit = 400
 		options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
 		
 		let fetchResult = PHAsset.fetchAssets(with: options)
 		
 		self.init(fetchResult: fetchResult)
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .done, target: self, action: #selector(filterImages))
 	}
 	
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+	
+	@objc func filterImages() {
+		remainingAssets = allAssets
+		filteredAssets = []
+		
+		filterNext()
+	}
+	
+	func filterNext() {
+		guard let asset = remainingAssets.popLast() else  {
+			print("done filtering")
+			return
+		}
+		
+		analyze(asset: asset) { [weak self] result in
+			guard let self = self else {
+				return
+			}
+			
+			switch result {
+			case .matches:
+				self.filteredAssets.append(asset)
+			case .noMatch(let reasons):
+				print("hiding image because of \(reasons.map({$0.rawValue}).joined(separator: ", "))")
+				break
+			}
+			
+			self.filterNext()
+		}
 	}
 	
 	override func viewDidLoad() {
@@ -86,7 +146,7 @@ class ViewController: UICollectionViewController {
         // Adjust the item size if the available width has changed.
         if availableWidth != width {
             availableWidth = width
-            let columnCount = (availableWidth / ViewController.minThumbnailSize).rounded(.towardZero)
+            let columnCount = (availableWidth / PhotoGridViewController.minThumbnailSize).rounded(.towardZero)
             let itemLength = (availableWidth - columnCount - 1) / columnCount
             layout.itemSize = CGSize(width: itemLength, height: itemLength)
             
@@ -99,7 +159,7 @@ class ViewController: UICollectionViewController {
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return fetchResult.count
+		return filteredAssets.count
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -107,7 +167,7 @@ class ViewController: UICollectionViewController {
 			fatalError("no cell?")
 		}
 		
-		let asset = fetchResult.object(at: indexPath.item)
+		let asset = filteredAssets[indexPath.item]
 		
 		cell.imageView.alpha = asset.location == nil ? 0.5 : 1.0
         cell.currentAssetIdentifier = asset.localIdentifier
@@ -126,13 +186,32 @@ class ViewController: UICollectionViewController {
 	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 		
 		
-		let asset = fetchResult.object(at: indexPath.item)
-		
-		if let location = asset.location {
-			let alert = UIAlertController(title: "Photo", message: "Latitude: \(location.coordinate.latitude)\nLongitude: \(location.coordinate.longitude)", preferredStyle: .alert)
+		let asset = filteredAssets[indexPath.item]
+		analyze(asset: asset) { [weak self] result in
+			var message = "Asset "
+			switch result {
+			case .matches:
+				message.append("matches!")
+			case .noMatch(let reasons):
+				message.append("does not match:\n")
+				for reason in reasons {
+					message.append("- ")
+					message.append(reason.rawValue)
+				}
+			}
+			
+			let alert = UIAlertController(title: "Photo", message: message, preferredStyle: .alert)
 			alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-			present(alert, animated: true, completion: nil)
+			self?.present(alert, animated: true, completion: nil)
 		}
+	}
+	
+	func analyze(asset: PHAsset, completion: @escaping (HistoricPhotoMatch) -> Void) {
+		guard asset.location != nil else {
+			completion(.noMatch([.noLocation]))
+			return
+		}
+		
 		let options = PHImageRequestOptions()
 		options.resizeMode = .none
 		options.isNetworkAccessAllowed = true
@@ -146,14 +225,14 @@ class ViewController: UICollectionViewController {
 			
 			print("Analyzing image...")
 			
-			self?.analyzeImage(asset: asset, data: data, orientation: orientation)
+			self?.analyzeImage(data: data, orientation: orientation, completion: completion)
 		}
 	}
 	
-	func analyzeImage(asset: PHAsset, data: Data, orientation: CGImagePropertyOrientation) {
+	func analyzeImage(data: Data, orientation: CGImagePropertyOrientation, completion: @escaping (HistoricPhotoMatch) -> Void) {
 		let imageRequestHandler = VNImageRequestHandler(data: data, orientation: orientation, options: [:])
 		
-		let classificationRequest = VNClassifyImageRequest { (request, error) in
+		let classificationRequest = VNClassifyImageRequest { [weak self] (request, error) in
 			guard let observations = request.results as? [VNClassificationObservation] else {
 				print("no observations?")
 				return
@@ -168,6 +247,8 @@ class ViewController: UICollectionViewController {
 				print("- \(observation.identifier)")
 			}
 			print("")
+			
+			self?.check(observations: confidentObservations, completion: completion)
 		}
 		
 		do {
@@ -175,7 +256,31 @@ class ViewController: UICollectionViewController {
 		} catch {
 			print("Error performing classification request: \(error.localizedDescription)")
 		}
+	}
+	
+	func check(observations: [VNClassificationObservation], completion: @escaping (HistoricPhotoMatch) -> Void) {
+		var reasons = [HistoricPhotoMatch.NoMatchReasons]()
 		
+		let containsPeople = observations.contains { $0.identifier == "people" }
+		if containsPeople {
+			// TODO: use Vision framework to see if their faces are recognizable
+			reasons.append(.containsPeople)
+		}
+		
+		let isOutdoors = observations.contains { $0.identifier == "outdoor" }
+		if !isOutdoors {
+			reasons.append(.notOutdoors)
+		}
+		let isDocument = observations.contains { $0.identifier == "document" }
+		if isDocument {
+			reasons.append(.isDocument)
+		}
+		
+		if reasons.count > 0 {
+			completion(.noMatch(reasons))
+		} else {
+			completion(.matches)
+		}
 	}
 }
 

3b029d7a6c37eea0ec15b9d2cc80a8f3b079c59a
Lieven Dekeyser <lievendekeyser@gmail.com>
2021-06-14 11:55:30 +0200

    Classify selected images using Vision.framework

--- PhotographicHistory/ViewController.swift
+++ PhotographicHistory/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import Photos
+import Vision
+
 
 class PhotoCell: UICollectionViewCell {
 	static let reuseIdentifier = "PhotoCell"
@@ -37,16 +39,19 @@ class PhotoCell: UICollectionViewCell {
 class ViewController: UICollectionViewController {
 	let fetchResult: PHFetchResult<PHAsset>
 	let imageManager = PHCachingImageManager()
-	    
+	var availableWidth: CGFloat = 0.0
+	
+	static let minThumbnailSize: CGFloat = 100.0
+	
 	let layout: UICollectionViewFlowLayout = {
 		let layout = UICollectionViewFlowLayout()
-		layout.itemSize = CGSize(width: 80.0, height: 80.0)
+		layout.itemSize = CGSize(width: ViewController.minThumbnailSize, height: ViewController.minThumbnailSize)
 		layout.minimumLineSpacing = 8.0
 		layout.minimumInteritemSpacing = 8.0
 		layout.sectionInset = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: 8.0)
 		return layout
 	}()
-
+	
 	init(fetchResult: PHFetchResult<PHAsset>) {
 		self.fetchResult = fetchResult
 		
@@ -55,7 +60,7 @@ class ViewController: UICollectionViewController {
 	
 	convenience init() {
 		let options = PHFetchOptions()
-		options.fetchLimit = 100
+		options.fetchLimit = 1000
 		options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
 		
 		let fetchResult = PHAsset.fetchAssets(with: options)
@@ -69,11 +74,25 @@ class ViewController: UICollectionViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		collectionView.insetsLayoutMarginsFromSafeArea = true
 		collectionView.backgroundColor = .white
 		
 		collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.reuseIdentifier)
 	}
+	
+	override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let width = view.bounds.inset(by: view.safeAreaInsets).width
+        // Adjust the item size if the available width has changed.
+        if availableWidth != width {
+            availableWidth = width
+            let columnCount = (availableWidth / ViewController.minThumbnailSize).rounded(.towardZero)
+            let itemLength = (availableWidth - columnCount - 1) / columnCount
+            layout.itemSize = CGSize(width: itemLength, height: itemLength)
+            
+            collectionView.reloadData()
+        }
+    }
 	
 	override func numberOfSections(in collectionView: UICollectionView) -> Int {
 		return 1
@@ -92,14 +111,71 @@ class ViewController: UICollectionViewController {
 		
 		cell.imageView.alpha = asset.location == nil ? 0.5 : 1.0
         cell.currentAssetIdentifier = asset.localIdentifier
-        imageManager.requestImage(for: asset, targetSize: CGSize(width: 160.0, height: 160.0), contentMode: .aspectFill, options: nil) { image, _ in
+        var targetSize = layout.itemSize
+        targetSize.width *= 2.0
+        targetSize.height *= 2.0
+        
+        imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFill, options: nil) { image, _ in
             if cell.currentAssetIdentifier == asset.localIdentifier {
                 cell.imageView.image = image
             }
         }
 		return cell
 	}
-
-
+	
+	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		
+		
+		let asset = fetchResult.object(at: indexPath.item)
+		
+		if let location = asset.location {
+			let alert = UIAlertController(title: "Photo", message: "Latitude: \(location.coordinate.latitude)\nLongitude: \(location.coordinate.longitude)", preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+			present(alert, animated: true, completion: nil)
+		}
+		let options = PHImageRequestOptions()
+		options.resizeMode = .none
+		options.isNetworkAccessAllowed = true
+		
+		print("Requesting image...")
+		imageManager.requestImageDataAndOrientation(for: asset, options: options) { [weak self] (data, _, orientation, _) in
+			guard let data = data else {
+				print("Couldn't get image")
+				return
+			}
+			
+			print("Analyzing image...")
+			
+			self?.analyzeImage(asset: asset, data: data, orientation: orientation)
+		}
+	}
+	
+	func analyzeImage(asset: PHAsset, data: Data, orientation: CGImagePropertyOrientation) {
+		let imageRequestHandler = VNImageRequestHandler(data: data, orientation: orientation, options: [:])
+		
+		let classificationRequest = VNClassifyImageRequest { (request, error) in
+			guard let observations = request.results as? [VNClassificationObservation] else {
+				print("no observations?")
+				return
+			}
+			
+			let confidentObservations = observations.filter {
+				$0.hasMinimumRecall(0.0, forPrecision: 0.9)
+			}
+			
+			print("Confident observations:")
+			for observation in confidentObservations {
+				print("- \(observation.identifier)")
+			}
+			print("")
+		}
+		
+		do {
+			try imageRequestHandler.perform([classificationRequest])
+		} catch {
+			print("Error performing classification request: \(error.localizedDescription)")
+		}
+		
+	}
 }
 
